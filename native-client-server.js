297001571aeec22bacffbdee75d06cc784c15892
ridhaginanjar <ridhaginanjar15@gmail.com>
2023-02-24 09:17:33 +0700

    add server code

--- native-client-server.js
+++ native-client-server.js
@@ -51,3 +51,12 @@ const requestListener = (request, response) => {
         }));
     }
 };
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
